9cf9d8874dbf63c2c3ee444e79f401f5cc8c56a6
Hugo Page <hpage@teamtalk.co.nz>
2020-04-28 09:46:32 +1200

    This is a new commit for what I originally planned to be amended

--- src/App.js
+++ src/App.js
@@ -14,7 +14,15 @@ class App extends Component {
     this.state = {}
   }
 
-
+  componentDidMount(){
+    var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+    var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+    s1.async=true;
+    s1.src='https://embed.tawk.to/5ea6b6e935bcbb0c9ab507ca/default';
+    s1.charset='UTF-8';
+    s1.setAttribute('crossorigin','*');
+    s0.parentNode.insertBefore(s1,s0);
+  }
 
   toggleNav = () => {
     document.getElementById('navbar').style.display = 'block'
