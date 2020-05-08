d4e67019f6706b5cfbf5a4dd084544fb590710dc
Hugo Page <hpage@teamtalk.co.nz>
2020-05-09 00:48:56 +1200

    new makeover

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -15,24 +15,26 @@ class Footer extends Component {
         <div style={{overflow:'auto',backgroundColor:'white'}}>
           <div class='footer-inner'>
             <img src={require('../images/veva-1.png')} style={{width:'96px',margin:'auto'}} id='footer-v-img' alt='Veva'/>
-            <ul class='bottom-nav' style={{textAlign:'center',float:'right'}}>
-              <li><a href="mailto:contact@vevadev.co.nz" id='mail-link'>contact@vevadev.co.nz</a></li>
-            </ul>
-            <ul  style={{float:'left'}}>
-              <Link to='/About' class='route-link-bottom'><li>About</li></Link>
-              <Link to='/Services' class='route-link-bottom'><li>Services</li></Link>
-              <Link to='/Contact' class='route-link-bottom'><li>Contact</li></Link>
-              <Link to='/OurTeam' class='route-link-bottom'><li>Our Team</li></Link>
-            </ul>
+            <div style={{width:'50%',margin:'auto'}}>
+              <ul class='bottom-nav' style={{textAlign:'center',float:'right'}}>
+                <li><a href="mailto:contact@vevadev.co.nz" id='mail-link'>contact@vevadev.co.nz</a></li>
+              </ul>
+              <ul  style={{float:'left'}}>
+                <Link to='/About' class='route-link-bottom'><li>About</li></Link>
+                <Link to='/Services' class='route-link-bottom'><li>Services</li></Link>
+                <Link to='/Contact' class='route-link-bottom'><li>Contact</li></Link>
+                <Link to='/OurTeam' class='route-link-bottom'><li>Our Team</li></Link>
+              </ul>
+            </div>
           </div>
         </div>
+        <p style={{textAlign:'center'}} class='copyright-tag'>Veva Development &#169;</p>
         <div class='footer-bottom'>
-        <ul class='social-tags'>
-          <SocialMediaIconsReact icon="linkedin" backgroundColor='none' roundness={0} class='social-link'/>
-          <SocialMediaIconsReact icon="facebook" backgroundColor='none' roundness={0} class='social-link'/>
-          <SocialMediaIconsReact icon="instagram" backgroundColor='none' roundness={0} class='social-link'/>
-        </ul>
-          <p style={{textAlign:'center'}} class='copyright-tag'>Veva Development &#169;</p>
+          <ul class='social-tags'>
+            <SocialMediaIconsReact icon="linkedin" backgroundColor='none' roundness={0} class='social-link'/>
+            <SocialMediaIconsReact icon="facebook" backgroundColor='none' roundness={0} class='social-link'/>
+            <SocialMediaIconsReact icon="instagram" backgroundColor='none' roundness={0} class='social-link'/>
+          </ul>
         </div>
       </div>
     )
--- src/services-data.js
+++ src/services-data.js
@@ -1,6 +1,7 @@
 export const data = [
   {
     name:'Business Development',
+    breif:null,
     keyPoints:[
       'Strategic Planning',
       'Sales Training',
@@ -12,6 +13,7 @@ export const data = [
   },
   {
     name:'Business Management',
+    brief:'Kick your feet back and relax! Our team of specialists are here to handle all the pain points of your business',
     keyPoints:[
       'Identify',
       'Analysing',
@@ -24,6 +26,7 @@ export const data = [
   },
   {
     name:'Investment Management',
+    brief:'With the latest technology and data resources at your disposal, managing your portfolios and ensuring the best results has never been easier!',
     keyPoints:[
       'Gain an online presence',
       'Expand your market online',
