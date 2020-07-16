31ef4348978025496daa133774d87d4624a7c864
Hugo Page <hpage@teamtalk.co.nz>
2020-07-16 17:52:08 +1200

    api-veva

--- src/components/Team.js
+++ src/components/Team.js
@@ -20,7 +20,7 @@ class Team extends Component {
     if(window.screen.width <= 560){
       document.getElementById('navbar').style.display = 'none'
     }
-    fetch('https://aoe1hjbasa.execute-api.us-east-1.amazonaws.com/dev/api/v1/team?id=qnQ4MWgHqScdXaRqi46IQmek1O62')
+    fetch('https://api.vevadev.co.nz/v1/team?id=qnQ4MWgHqScdXaRqi46IQmek1O62')
     .then(response => response.json())
     .then((data) => {
       this.setState({data})
